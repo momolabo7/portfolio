ccb721bd6cd503c13bf3f02b97fce2af93e06fac
GERALD WONG <ramza88@gmail.com>
2021-02-20 17:03:15 +0800

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,6 @@ import React from 'react'
 function Footer() {
     return (
         <footer>
-            <p className="footer-text">Stay Connected!</p>
-            
-            <p>TODO:contact here</p>
             <p className="footer-copyright-text">
                 © 2020 Gerald Wong 
             </p> 
